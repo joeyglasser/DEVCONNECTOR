7fbe266691b0ba7cd23b82603e509310e9a65c2d
joeyglasser <josephmglasser@gmail.com>
2022-01-25 20:44:46 -0500

    made actions/reducers for getting profiles/repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,7 +1,9 @@
 import {
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
+  GET_REPOS,
   UPDATE_PROFILE,
 } from "../actions/types";
 
@@ -22,6 +24,18 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     case PROFILE_ERROR:
